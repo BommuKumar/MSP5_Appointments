ce0ae80002fa8261edad40d9e9461a02bc11ea56
BommuKumar <swamylucky8899@gmail.com>
2024-08-24 22:26:48 +0530

    project creation

---
+++ MSP5_Appointment_App/src/main/java/com/naresh/Database/Msp4AppointmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.naresh.Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msp4AppointmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msp4AppointmentAppApplication.class, args);
+	}
+
+}
